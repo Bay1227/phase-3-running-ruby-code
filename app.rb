7e21a84284c8cbd85f27300892f0de3037eb9e77
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-08-29 13:13:14 -0700

    ruby

---
+++ app.rb
@@ -0,0 +1,15 @@
+# puts "Hello World!" // print Hello World!" in the console"
+# puts "Hello World!"
+# puts "Hello World!"
+
+# print "Hello World!" // print helo wolrd in the console but % sign in the back
+
+# puts [1, 2, 3] print 1, 2, 3, in the console as a listed
+
+# p [1, 2, 3] The p method will output the data in a nicer format by calling the .inspect method on our data
+
+# pp [{id:1, hello: "World"}, {id:2, hello: "Ruby"}, {id:3, hello: "Moon"}, {id:4, hello: "Learner"}] //The pp method will "pretty-print" these objects by calling the .pretty_inspect method on our data.
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3] 
